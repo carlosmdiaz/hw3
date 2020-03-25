7babec20fe5f46620025cd7b6691d22adc50e33d
kristel-spike <kristelb@macbook-air.home>
2020-03-25 02:00:10 -0400

    added drag

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Midboard from './components/MidBoard'
+import DesignContainer from './components/DesignContainer'
 //import {MidBoard} from './components/MidBoard.js (NOT WORKING)'
 
 //import component js pages here
@@ -16,6 +17,7 @@ function App() {
   
       <header className="App-header">
       <Midboard/>
+      <DesignContainer/>
       </header>
     </div>
   );
