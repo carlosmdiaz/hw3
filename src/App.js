e832e9f391f194d75c1c069cb101245128d2aff5
carlosmdiaz <carlosdiaz3979@gmail.com>
2020-03-31 18:46:33 -0400

    This commit changes the MidBoard.js and divided it into different components. Finally working.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import DesingContainer from "./components/DesignContainer";
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Midboard from './components/MidBoard'
-//import DnD from './components/DnD'
-//import {MidBoard} from './components/MidBoard.js (NOT WORKING)'
-
-//import component js pages here
->>>>>>> ec8b6adc50649f8db14129338e0388f1c645ce3e
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <DesingContainer />
-=======
-  
-      <header className="App-header">
-        <Midboard/>
-      </header>
->>>>>>> ec8b6adc50649f8db14129338e0388f1c645ce3e
     </div>
   );
 }
